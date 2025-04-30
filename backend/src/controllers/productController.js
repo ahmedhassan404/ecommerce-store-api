fd557120cbe5836a812b8f51314a3b3b83f07259
gawishh <abdelrhman.jawish@gmail.com>
2025-04-30 13:40:34 +0300

    Your clear and short commit message

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -111,14 +111,13 @@ const addProduct = async (req, res, next) => {
   }
 };
 
-const getProductsByCategory = async (req, res) => {
+const getProductsByCategory = async (req, res, next) => {
   const { category } = req.params;
   try {
     const products = await Product.find({ category });
     res.json({ products });
   } catch (error) {
-    console.log("Error in getProductsByCategory controller", error.message);
-    res.status(500).json({ message: "Server error", error: error.message });
+    next(error);
   }
 };
 
